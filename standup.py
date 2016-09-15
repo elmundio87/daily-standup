76d06759ddc18eb4d02aa97395742ee18860e2d9
Edmund Dipple <elmundio1987@gmail.com>
2016-09-15 19:27:17 +0100

    Pull in list of blocked issues

--- standup.py
+++ standup.py
@@ -1,23 +1,36 @@
 import requests
 import lxml.html
 import config
+from jira import JIRA
 
-url = "https://devopsguys.atlassian.net/wiki/display/DEVOPSGUYS/Pack+2+Sprint+{0}+Retrospective".format(config.sprint)
+def getSprintGoals():
+	url = "https://devopsguys.atlassian.net/wiki/display/DEVOPSGUYS/Pack+2+Sprint+{0}+Retrospective".format(config.sprint)
 
-r = requests.get(url, auth=(config.user, config.password))
-r.raise_for_status
+	r = requests.get(url, auth=(config.user, config.password))
+	r.raise_for_status
 
-rootElement = None
+	rootElement = None
 
-tree = lxml.html.fromstring(r.text)
+	tree = lxml.html.fromstring(r.text)
 
-elements = tree.find_class("innerCell")
+	elements = tree.find_class("innerCell")
 
-for element in elements:
-	if "SprintGoals" in lxml.html.tostring(element):
-		rootElement = element
+	for element in elements:
+		if "SprintGoals" in lxml.html.tostring(element):
+			rootElement = element
 
-goals = rootElement.find('ul').findall('li')
+	goals = rootElement.find('ul')
+	return lxml.html.tostring(goals)
 
-for goal in goals:
-	print(lxml.html.tostring(goal))
+def getBlockedIssues():
+	issues = ""
+	jira = JIRA('https://devopsguys.atlassian.net', basic_auth=(config.user, config.password))
+	blocked = all_proj_issues_but_mine = jira.search_issues('Flagged = Impediment AND Sprint = "Pack 2 Sprint #{0}"'.format(config.sprint))
+	for issue in blocked:
+		issues += issue.key
+		issues += " : "
+		issues += issue.fields.summary
+	print issues
+
+getBlockedIssues()
+getSprintGoals()
