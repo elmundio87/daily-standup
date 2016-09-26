714b110daa1d812ba215b85c5d887faf9fdb604e
Edmund Dipple <elmundio1987@gmail.com>
2016-09-26 18:45:32 +0100

    Split out Blocked issues from With Customer

--- static/js/blockedIssues.js
+++ static/js/blockedIssues.js
@@ -1,6 +1,7 @@
 blockedIssuesHandler = function(data){
   
   blocked_issue_cards = $("#blocked_issue_cards").clone().empty()
+  with_customer_issue_cards = $("#with_customer_issue_cards").clone().empty()
 
   json = JSON.parse(data)
   
@@ -13,35 +14,61 @@ blockedIssuesHandler = function(data){
     
     issue = json.issues[entry]
     
-    card = $('<div>').attr('class','card')
-    id_link = $('<a>')
-    id_link.attr('target','_blank')
-    id_link.attr('href',json.base_url + "/browse/" + issue.key)
-    id_link.text(issue.key)
     
-    jira_id = $('<div>').attr('class', 'card_jira_id')
-    jira_id.append(id_link)
-    
-    jira_description = $('<div>')
-    jira_description.attr('class','card_jira_description')
-    jira_description.html(issue.description.replace("Why","</br>Why"))
-    
-    jira_status = $('<div>')
-    jira_status.attr('class','card_jira_status')
-    jira_status.text(issue.status)
-    if(issue.flagged){
-      jira_status.addClass('flagged')
+    if(issue.status == "With Customer"){
+      card = $('<div>').attr('class','minicard')
+      id_link = $('<a>')
+      id_link.attr('target','_blank')
+      id_link.attr('href',json.base_url + "/browse/" + issue.key)
+      id_link.text(issue.key)
+      
+      jira_id = $('<div>').addClass("jira_id")
+      jira_id.append(id_link)
+      
+      jira_description = $('<div>').addClass("jira_description")
+      jira_description.html(issue.description)
+      
+      with_customer_issue_cards.append(card)
+      
+      card.append(jira_id)
+      card.append(jira_description)
+      
+      
+    }else{
+      card = $('<div>').attr('class','card')
+      id_link = $('<a>')
+      id_link.attr('target','_blank')
+      id_link.attr('href',json.base_url + "/browse/" + issue.key)
+      id_link.text(issue.key)
+      
+      jira_id = $('<div>').attr('class', 'card_jira_id')
+      jira_id.append(id_link)
+      
+      jira_description = $('<div>')
+      jira_description.attr('class','card_jira_description')
+      jira_description.html(issue.description.replace("Why","</br>Why"))
+      
+      jira_status = $('<div>')
+      jira_status.attr('class','card_jira_status')
+      jira_status.text(issue.status)
+      if(issue.flagged){
+        jira_status.addClass('flagged')
+      }
+      
+      card.append(jira_id)
+      card.append(jira_status)
+      card.append(jira_description)
+      blocked_issue_cards.append(card)
     }
     
-    card.append(jira_id)
-    card.append(jira_status)
-    card.append(jira_description)
-    
-    blocked_issue_cards.append(card)
   }
   
   if( blocked_issue_cards.html() != $("#blocked_issue_cards").html()){
     $("#blocked_issue_cards").html(blocked_issue_cards.html())
+  }
+  
+  if( with_customer_issue_cards.html() != $("#with_customer_issue_cards").html()){
+    $("#with_customer_issue_cards").html(with_customer_issue_cards.html())
   }
   
 }
