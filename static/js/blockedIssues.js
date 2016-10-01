91d0b8ade4a2099033af9624cad39edb5193220f
Edmund Dipple <elmundio1987@gmail.com>
2016-10-01 18:15:29 +0300

    integrate the with-customer tickets better into the panel

--- static/js/blockedIssues.js
+++ static/js/blockedIssues.js
@@ -1,77 +1,71 @@
 blockedIssuesHandler = function(data){
-  
+
   blocked_issue_cards = $("#blocked_issue_cards").clone().empty()
   with_customer_issue_cards = $("#with_customer_issue_cards").clone().empty()
 
   json = JSON.parse(data)
-  
+
   if(json.hasOwnProperty('error')){
     $("#blocked_issue_cards").text(json['error'])
     $("#with_customer_issue_cards").text(json['error'])
     return
   }
-  
+
   for (var entry in json.issues) {
-    
+
     issue = json.issues[entry]
-    
-    
+
+
     if(issue.status == "With Customer"){
-      card = $('<div>').attr('class','minicard')
+      card = $('<li>').attr('class','list-group-item')
+
       id_link = $('<a>')
+      id_link.attr('class','with-customer-jira-id')
       id_link.attr('target','_blank')
       id_link.attr('href',json.base_url + "/browse/" + issue.key)
       id_link.text(issue.key)
-      
-      jira_id = $('<div>').addClass("jira_id")
-      jira_id.append(id_link)
-      
-      jira_description = $('<div>').addClass("jira_description")
-      jira_description.html(issue.description)
-      
+
+      card.append(id_link)
+      card.append(issue.description)
       with_customer_issue_cards.append(card)
-      
-      card.append(jira_id)
-      card.append(jira_description)
-      
-      
+
     }else{
       card = $('<div>').attr('class','card')
       id_link = $('<a>')
       id_link.attr('target','_blank')
       id_link.attr('href',json.base_url + "/browse/" + issue.key)
       id_link.text(issue.key)
-      
+
       jira_id = $('<div>').attr('class', 'card_jira_id')
       jira_id.append(id_link)
-      
+
       jira_description = $('<div>')
       jira_description.attr('class','card_jira_description')
       jira_description.html(issue.description.replace("Why","</br>Why"))
-      
+
       jira_status = $('<div>')
       jira_status.attr('class','card_jira_status')
       jira_status.text(issue.status)
       if(issue.flagged){
         jira_status.addClass('flagged')
       }
-      
+
       card.append(jira_id)
       card.append(jira_status)
       card.append(jira_description)
       blocked_issue_cards.append(card)
     }
-    
+
   }
-  
+
   if( blocked_issue_cards.html() != $("#blocked_issue_cards").html()){
     $("#blocked_issue_cards").html(blocked_issue_cards.html())
   }
-  
+
   if( with_customer_issue_cards.html() != $("#with_customer_issue_cards").html()){
     $("#with_customer_issue_cards").html(with_customer_issue_cards.html())
   }
-  
+
 }
 
 getBlockedIssues = function(){
@@ -88,5 +82,5 @@ getBlockedIssues = function(){
     $(".message").html("Rendering Blocked Issues")
     $("#loader").addClass("invisible")
   })
-      
+
 }
