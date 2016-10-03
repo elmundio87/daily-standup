341321eaa6b439a67286919c0a69e5151b335d68
elmundio87 <elmundio1987@gmail.com>
2016-10-03 19:05:22 +0100

    Updated sprint actions to include the SSL certs

--- static/js/sprintActions.js
+++ static/js/sprintActions.js
@@ -1,3 +1,12 @@
+var sprint_actions = ""
+var expiring_certs = ""
+
+updateSprintActions = function(){
+    $("#sprint-actions").html(sprint_actions + expiring_certs)
+    $("#sprint-actions").find("br").remove()
+    fitToPanel("sprint-actions")
+}
+
 getSprintActions = function(){
     
   getLastSprintName = function(sprint_name){
@@ -21,10 +30,32 @@ getSprintActions = function(){
     data: { sprint_name: getLastSprintName(SPRINT_NAME) }
   }).done(function (data){
     $(".message").html("Rendering Sprint Actions")
-    $("#sprint-actions").html(data)
-    $("#sprint-actions").find("br").remove()
-    fitToPanel("sprint-actions")
+    sprint_actions = data
+    updateSprintActions()
   })
     
+  $.ajax({
+    type: "GET",
+    url: API_URL + "/getExpiringCertificates",
+    data: { board_name: board_name }
+  }).done(function (data){
+    $(".message").html("Finding expiring certificates")
+
+    json = JSON.parse(data)
+    root = $('<ul>')
+  
+    for (var item in json){
+      host = json[item]
+      if( host.days_remaining <= 400){
+        listitem = $("<li>")
+        listitem.css("font-weight", "bold")
+        listitem.text("SSL certificate for " + host.hostname + " will expire in " + host.days_remaining  + " days!")
+        root.append(listitem)
+      }
+    }
+    
+    expiring_certs = root[0].outerHTML
+    updateSprintActions()
+  })
   
 }
