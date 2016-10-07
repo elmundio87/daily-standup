026ef85e76e7c3809b5886f905edbeb7bc93f209
elmundio87 <elmundio1987@gmail.com>
2016-10-07 19:14:42 +0100

    pep8 linting changes

--- lib/expiring_certs.py
+++ lib/expiring_certs.py
@@ -6,6 +6,7 @@ from config import expiring_certs_config as config
 from pyasn1_modules import pem, rfc2459
 from pyasn1.codec.der import decoder as der_decoder
 
+
 def get_expiring_certs(board_name):
     output = []
     if board_name in config.sites.keys():
@@ -13,21 +14,28 @@ def get_expiring_certs(board_name):
             output.append(ssl_output_item(site))
     return json.dumps(output)
 
+
 def ssl_output_item(hostname):
     expire_date, error = ssl_expiry_datetime(hostname)
-    return {"hostname": hostname, "days_remaining": ssl_valid_time_remaining(hostname), "notAfter": "{:%B %d, %Y}".format(expire_date), "error": error}
+    return {"hostname": hostname,
+            "days_remaining": ssl_valid_time_remaining(hostname),
+            "notAfter": "{:%B %d, %Y}".format(expire_date),
+            "error": error
+            }
+
 
 def ssl_expiry_datetime(hostname):
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
 
     pem_cert = ssl.get_server_certificate((hostname, 443))
     der_cert = ssl.PEM_cert_to_DER_cert(pem_cert)
-    cert = der_decoder.decode(der_cert,asn1Spec=rfc2459.Certificate())[0]
+    cert = der_decoder.decode(der_cert, asn1Spec=rfc2459.Certificate())[0]
     tbs = cert.getComponentByName('tbsCertificate')
     validity = tbs.getComponentByName('validity')
     not_after = validity.getComponentByName('notAfter').getComponent()
 
-    return datetime.datetime.strptime(str(not_after), '%y%m%d%H%M%SZ'),""
+    return datetime.datetime.strptime(str(not_after), '%y%m%d%H%M%SZ'), ""
+
 
 def ssl_valid_time_remaining(hostname):
     """Get the number of days left in a cert's lifetime."""
--- server.py
+++ server.py
@@ -14,20 +14,24 @@ from lib import expiring_certs
 
 app = Flask(__name__)
 
+
 def is_working_day(date):
     return date.weekday() <= 4
+
 
 def get_working_days(date_start_obj, date_end_obj):
 
     total_working_days = 0
 
-    date_range = [date_start_obj + timedelta(days=x) for x in range(0, (date_end_obj-date_start_obj).days)]
+    date_range = [date_start_obj + timedelta(days=x)
+                  for x in range(0, (date_end_obj - date_start_obj).days)]
 
     for date in date_range:
         if is_working_day(date):
             total_working_days += 1
 
     return total_working_days
+
 
 def check_auth(username, password):
     """This function is called to check if a username /
@@ -38,12 +42,14 @@ def check_auth(username, password):
     auth_password_sha256 = h.hexdigest()
     return username == config.auth_username and auth_password_sha256 == config.auth_password_sha256
 
+
 def authenticate():
     """Sends a 401 response that enables basic auth"""
     return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def requires_auth(f):
     @wraps(f)
@@ -54,17 +60,21 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
+
 @app.route('/')
 @requires_auth
 def index():
     return "Hello world", 200
 
+
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
     return response
+
 
 @app.route("/getBlockedIssues")
 @requires_auth
@@ -75,23 +85,34 @@ def getBlockedIssues():
     else:
         return 'getBlockedIssues requires parameter [sprint_name]', 400
 
-    jira = JIRA(config.base_url, basic_auth=(config.atlassian_username, config.atlassian_password))
-    flagged  = jira.search_issues('Status NOT IN (Closed, Resolved) AND Flagged = Impediment AND Sprint = "{0}"'.format(sprint_name))
-    with_customer = jira.search_issues('Status NOT IN (Closed, Resolved) AND Status = "With Customer" AND Sprint = "{0}"'.format(sprint_name))
+    jira = JIRA(config.base_url, basic_auth=(
+        config.atlassian_username, config.atlassian_password))
+    flagged = jira.search_issues(
+        'Status NOT IN (Closed, Resolved) AND Flagged '
+        '= Impediment AND Sprint = "{0}"'.format(sprint_name))
+    with_customer = jira.search_issues(
+        'Status NOT IN (Closed, Resolved) AND Status = "With Customer" AND '
+        'Sprint = "{0}"'.format(sprint_name))
 
     issues = []
 
     for issue in flagged:
-        issues.append({"key":issue.key, "status":issue.fields.status.name, "description":issue.fields.summary, "flagged": True})
+        issues.append({"key": issue.key,
+                       "status": issue.fields.status.name,
+                       "description": issue.fields.summary,
+                       "flagged": True})
 
     for issue in with_customer:
-        issues.append({"key":issue.key, "status":issue.fields.status.name, "description":issue.fields.summary, "flagged": False})
-
+        issues.append({"key": issue.key, "status": issue.fields.status.name,
+                       "description": issue.fields.summary, "flagged": False})
 
     if len(issues) == 0:
-        return json.dumps({"error": "No blocked issues found. Any issue that is flagged or 'With Customer' is counted as 'Blocked'"})
+        return json.dumps({"error": "No blocked issues found. Any issue that "
+                           "is flagged or 'With Customer' is counted as "
+                           "'Blocked'"})
 
-    return json.dumps({"issues":issues,"base_url":config.base_url}), 200
+    return json.dumps({"issues": issues, "base_url": config.base_url}), 200
+
 
 @app.route("/getSprintGoals")
 @requires_auth
@@ -102,8 +123,10 @@ def getSprintGoals():
     else:
         return 'getSprintGoals requires parameter [sprint_name]'
 
-    url = "{0}/wiki/display/DEVOPSGUYS/{1}+Retrospective".format(config.base_url, sprint_name.replace(" ","+").replace("#",""))
-    r = requests.get(url, auth=(config.atlassian_username, config.atlassian_password))
+    url = "{0}/wiki/display/DEVOPSGUYS/{1}+Retrospective".format(
+        config.base_url, sprint_name.replace(" ", "+").replace("#", ""))
+    r = requests.get(url, auth=(
+        config.atlassian_username, config.atlassian_password))
     r.raise_for_status
 
     tree = lxml.html.fromstring(r.text)
@@ -114,12 +137,13 @@ def getSprintGoals():
 
     for element in elements:
         if ">Sprint Goals<" in lxml.html.tostring(element):
-        	goals = element.find('ul')
+            goals = element.find('ul')
 
-    if goals == None:
+    if goals is None:
         return "No Sprint goals found in {0}. Please ensure that {0} exists, and that there is a section called 'Sprint Goals' that contains a list.".format(url), 200
 
     return lxml.html.tostring(goals), 200
+
 
 @app.route("/getSprintActions")
 @requires_auth
@@ -130,8 +154,10 @@ def getSprintActions():
     else:
         return 'getSprintActions requires parameter [sprint_name]'
 
-    url = "{0}/wiki/display/DEVOPSGUYS/{1}+Retrospective".format(config.base_url, sprint_name.replace(" ","+").replace("#",""))
-    r = requests.get(url, auth=(config.atlassian_username, config.atlassian_password))
+    url = "{0}/wiki/display/DEVOPSGUYS/{1}+Retrospective".format(
+        config.base_url, sprint_name.replace(" ", "+").replace("#", ""))
+    r = requests.get(url, auth=(config.atlassian_username,
+                                config.atlassian_password))
     r.raise_for_status
 
     tree = lxml.html.fromstring(r.text)
@@ -142,12 +168,13 @@ def getSprintActions():
 
     for element in elements:
         if ">Actions<" in lxml.html.tostring(element):
-        	goals = element.find('ul')
+            goals = element.find('ul')
 
-    if goals == None:
+    if goals is None:
         return "No Sprint actions found in Confluence. Please ensure that <a href='{0}'>this page</a> exists, and that there is a section called 'Actions' that contains a list.".format(url), 200
 
     return lxml.html.tostring(goals), 200
+
 
 @app.route("/getRapidBoardId")
 @requires_auth
@@ -159,12 +186,14 @@ def getRapidBoardId():
         return 'getRapidBoardId requires parameter [board_name]'
 
     url = "{0}/rest/greenhopper/1.0/rapidview".format(config.base_url)
-    r = requests.get(url, auth=(config.atlassian_username, config.atlassian_password))
+    r = requests.get(url, auth=(
+        config.atlassian_username, config.atlassian_password))
     boards = json.loads(r.text)
     for board in boards['views']:
         if board['name'] == board_name:
             return json.dumps({"board_id": board['id']}), 200
     return "No matching board found", 200
+
 
 @app.route("/getSprintDaysRemaining")
 @requires_auth
@@ -180,14 +209,20 @@ def getSprintDaysRemaining():
     else:
         return 'getSprintEndDate requires parameter [sprint_id]', 400
 
-    url = "{0}/rest/greenhopper/1.0/rapid/charts/sprintreport/?rapidViewId={1}&sprintId={2}".format(config.base_url, board_id, sprint_id)
-    r = requests.get(url, auth=(config.atlassian_username, config.atlassian_password))
+    url = "{0}/rest/greenhopper/1.0/rapid/charts/sprintreport/?rapidViewId={1}&sprintId={2}".format(
+        config.base_url, board_id, sprint_id)
+    r = requests.get(url, auth=(
+        config.atlassian_username,
+        config.atlassian_password)
+    )
     sprint_report = json.loads(r.text)
 
     startDate = date.today()
-    endDate = datetime.strptime(sprint_report["sprint"]["endDate"], '%d/%b/%y %H:%M %p').date()
+    endDate = datetime.strptime(sprint_report["sprint"][
+                                "endDate"], '%d/%b/%y %H:%M %p').date()
 
     return "{0}".format(get_working_days(startDate, endDate)), 200
+
 
 @app.route("/getSprintName")
 @requires_auth
@@ -203,14 +238,19 @@ def getSprintName():
     else:
         return 'getRapidBoardId requires parameter [board_name]', 400
 
-    url = "{0}/rest/greenhopper/latest/sprintquery/{1}".format(config.base_url, board_id)
-    r = requests.get(url, auth=(config.atlassian_username, config.atlassian_password))
+    url = "{0}/rest/greenhopper/latest/sprintquery/{1}".format(
+        config.base_url, board_id)
+    r = requests.get(url, auth=(
+        config.atlassian_username, config.atlassian_password))
     sprints = json.loads(r.text)
     for sprint in sprints['sprints']:
         if sprint['state'] == "ACTIVE" and board_name in sprint['name']:
-            return json.dumps({"sprint_name": sprint['name'], "sprint_id": sprint['id']}), 200
+            return json.dumps({
+                "sprint_name": sprint['name'],
+                "sprint_id": sprint['id']}), 200
 
     return "No matching sprint found", 400
+
 
 @app.route("/getExpiringCertificates")
 def getExpiringCertificates():
