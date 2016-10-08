a5e56ee979aa3a0af57d767b97a6a3bce92b81d5
elmundio87 <elmundio1987@gmail.com>
2016-10-08 17:22:56 +0100

    updates to deploy-live static assets

--- deploy-live.sh
+++ deploy-live.sh
@@ -11,4 +11,4 @@ echo "var API_URL = \"${API_URL}\"" > static/properties/urls.js
 aws_access_key_id="$(aws configure get aws_access_key_id --profile daily-standup)"
 aws_secret_access_key="$(aws configure get aws_secret_access_key --profile daily-standup)"
 
-s3cmd sync static/ s3://static-dog-dailystandup-live/ --access_key ${aws_access_key_id} --secret_key ${aws_secret_access_key}
+s3cmd sync static/ s3://static-dog-dailystandup-live/ --delete-removed --no-mime-magic --guess-mime-type --access_key ${aws_access_key_id} --secret_key ${aws_secret_access_key}
