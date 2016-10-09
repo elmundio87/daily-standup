2f182e02c42498a873be0d3be8c796962d0c0c1c
elmundio87 <elmundio1987@gmail.com>
2016-10-09 16:54:41 +0100

    Try to load the certs configuration file to catch any errors before deployment

--- config/expiring_certs_config.py
+++ config/expiring_certs_config.py
@@ -3,19 +3,19 @@ sites = {}
 sites["Pack 1"] = []
 
 sites["Pack 2"] = [
-"devopsguys.com",
-"staging.apmg-businessbooks.com",
-"apmg-businessbooks.com",
-"apmg-cyber.com",
-"apmg-certified.com",
-"urm.apmg-certified.com",
-"bureauveritas.apmg-certified.com",
-"staging.apmg-cyber.com",
-"staging.apmg-certified.com",
-"staging-urm.apmg-certified.com",
-"staging-bureauveritas.apmg-certified.com",
-"staging.ppp-certification.com",
-"ppp-certification.com",
-"cdcat-apmg.com",
-"preprod.cdcat-apmg.com"
+    "devopsguys.com",
+    "staging.apmg-businessbooks.com",
+    "apmg-businessbooks.com",
+    "apmg-cyber.com",
+    "apmg-certified.com",
+    "urm.apmg-certified.com",
+    "bureauveritas.apmg-certified.com",
+    "staging.apmg-cyber.com",
+    "staging.apmg-certified.com",
+    "staging-urm.apmg-certified.com",
+    "staging-bureauveritas.apmg-certified.com",
+    "staging.ppp-certification.com",
+    "ppp-certification.com",
+    "cdcat-apmg.com"
+    "preprod.cdcat-apmg.com"
 ]
