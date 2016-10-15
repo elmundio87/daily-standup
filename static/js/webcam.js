d29fee13d4c1ef4e174aaeae1a7b08d7b95fdfb1
elmundio87 <elmundio1987@gmail.com>
2016-10-15 12:01:21 +0100

    use up the extra space when the webcam isn't showing

--- static/js/webcam.js
+++ static/js/webcam.js
@@ -3,11 +3,17 @@ var videoShouldShow = getUrlParameter('video');
 
 if(videoShouldShow != "on"){
   $("#webcam").hide();
+  $("#panel-sprint-goals").addClass("no-webcam");
+  $("#panel-sprint-actions").addClass("no-webcam");
 }else{
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
   if (navigator.getUserMedia) {
   	navigator.getUserMedia({video: true}, handleVideo, videoError);
+  }else{
+    $("#webcam").hide();
+    $("#panel-sprint-goals").addClass("no-webcam");
+    $("#panel-sprint-actions").addClass("no-webcam");
   }
 	
 }
